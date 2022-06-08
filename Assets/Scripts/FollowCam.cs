df6d037008f99f1f3ea2d816ede49f096f004e45
Nazar <nazar.konechniy2@gmail.com>
2022-06-08 23:53:38 +0300

    first commit

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+
+    static public GameObject POI;
+    public float easing = 0.05f;
+    public float camZ;
+    public Vector2 minXY = Vector2.zero;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 destination;
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            destination = POI.transform.position;
+            if (POI.tag == "Projectile")
+            {
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    POI = null;
+                    return;
+                }
+            }
+        }
+        // Ограничить X и Y минимальными значениями
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
+    }
+}
